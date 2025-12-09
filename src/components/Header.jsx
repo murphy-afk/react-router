a8d65e606002049a1d3c81dfc78e529998cf2eeb
murphy-afk <bardarenoemi@gmail.com>
2025-12-09 17:34:36 +0100

    feat: logo and icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,28 @@
 import { NavLink } from "react-router-dom";
+import Logo from '../assets/Logo.png'
 
 export default function Header() {
   return (
     <>
-      <nav className="d-flex gap-2 py-3 bg-dark text-light ps-4">
-        <NavLink className='nav-link' to='/'>Home</NavLink>
-        <NavLink className='nav-link active' to='/about-us'>About us</NavLink>
-        <NavLink className='nav-link' to='/products'>Products</NavLink>
+      <nav className="d-flex bg-dark text-light ps-4 align-items-center justify-content-between">
+        <div className="header-right d-flex gap-2 py-3 ps-4 align-items-center">
+
+          <a className="navbar-brand" href="#">
+            <img src={Logo} alt="Logo" />
+          </a>
+          <NavLink className='nav-link' to='/'>Home</NavLink>
+          <NavLink className='nav-link active' to='/about-us'>About us</NavLink>
+          <NavLink className='nav-link' to='/products'>Products</NavLink>
+        </div>
+        <div className="header-left pe-4">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart me-2" viewBox="0 0 16 16">
+            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart" viewBox="0 0 16 16">
+            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+          </svg>
+        </div>
+
       </nav>
     </>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,12 @@
-import { NavLink } from "react-router-dom";
 import Carousel from "../components/Carousel";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <>
-      <div className="container d-flex flex-column align-items-center ">
-
-        <div className="btn-container d-flex justify-content-around w-50 pt-2">
-          <NavLink className='btn border' to='/about-us'>About us</NavLink>
-          <NavLink className='btn border' to='/products'>Products</NavLink>
-        </div>
-        <h1 className="text-center pt-5 pb-2">Welcome to Boomazon</h1>
+      <Header />
+      <div className="container home-container d-flex flex-column align-items-center w-100 m-0 p-0">
+        <h1 className="text-center pt-0 pb-2 bg-secondary w-100">Welcome to Boomazon</h1>
         <Carousel />
       </div>
     </>
