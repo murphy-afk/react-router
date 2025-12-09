f67a7b2b87649db241ff4c8c95e8ce5b2dc17b93
murphy-afk <bardarenoemi@gmail.com>
2025-12-09 15:27:10 +0100

    feat: product card

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -5,24 +5,38 @@ import { useEffect, useState } from "react";
 export default function Products() {
   const [products, setProducts] = useState([])
 
-  useEffect(() => 
+  useEffect(() =>
     getProducts()
-  , [])
+    , [])
 
   function getProducts() {
-    axios .get('https://fakestoreapi.com/products')
-    .then((resp) => {
-      const prodData = resp.data;
-      setProducts(prodData);
-      console.log(prodData);
-      
-    })
+    axios.get('https://fakestoreapi.com/products')
+      .then((resp) => {
+        const prodData = resp.data;
+        setProducts(prodData);
+        console.log(prodData);
+
+      })
   }
-  return(
+  return (
     <>
-    <Header />
-    <h1>Products</h1>
-    {}
+      <Header />
+      <h1>Products</h1>
+      <div className="container">
+        <div className="row row-cols-3">
+          {products.map(product => (
+            <div className="card" key={product.id}>
+              <div className="card-img-top">
+                <img src={product.image} alt="" />
+                <div className="card-body">
+                  <div className="card-title">{product.title}</div>
+                  <div className="card-text">{product.description}</div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
