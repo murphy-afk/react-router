d3ed9014d71d4c6391741cf621ba97fb666bd252
murphy-afk <bardarenoemi@gmail.com>
2025-12-10 15:05:15 +0100

    feat: product page content

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,11 +1,27 @@
 import { useParams } from "react-router-dom";
 import Card from "../components/Card";
+import { useEffect, useState } from "react";
+import axios from 'axios'
+import Header from "../components/Header";
 
 export default function SingleProduct() {
   const {id} = useParams();
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    axios .get(`https://fakestoreapi.com/products/${id}`)
+    .then((resp) => {
+      setProduct(resp.data)
+    })
+  }, [id])
 
   return(
-    <h1>product page</h1>
-    // <Card />
+    <>
+    <Header />
+    <h1 className="text-light text-center">Product detais</h1>
+    <div className="container">
+    <Card product={product}/>
+    </div>
+    </>
   )
 }
